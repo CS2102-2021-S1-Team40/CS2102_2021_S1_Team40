7b6afce3a430b9bd3df95b74e37bbd4bb575ede9
Sheryl Tay <sheryltayyyy@gmail.com>
2020-11-05 01:10:31 +0800

    edited dropdown in bids to show pet names instead

--- public/src/components/Bid.js
+++ public/src/components/Bid.js
@@ -31,50 +31,34 @@ export default function Bid(props) {
 
   const [start_date, setStartDate] = useState(new Date());
   const [end_date, setEndDate] = useState(start_date);
-  const [pet_type, setPetType] = useState("");
+  const [pet_name, setPetName] = useState("");
+  // const [pet_type, setPetType] = useState("");
   const [price, setPrice] = useState(0);
   const [transfer_method, setTransferMethod] = useState("");
   const [payment_method, setPaymentMethod] = useState("");
-  const [send_bid, setSendBid] = useState(false);
+  // const [send_bid, setSendBid] = useState(false);
   const petowner_username = user.username;
-  const pet = useSelector(selectPet);
-
-  useEffect(() => {
-    if (open) {
-      dispatch(getPetName(petowner_username, pet_type));
-    }
-  }, [pet_type, caretaker]);
-
-  const today = new Date();
-  const today_date = `${today.getFullYear()}-${
-    today.getMonth() + 1
-  }-${today.getDate()}`;
-  const two_years_later_date = `${today.getFullYear() + 2}-${
-    today.getMonth() + 1
-  }-${today.getDate()}`;
+  const pet_names = useSelector(selectPet);
 
   // useEffect(() => {
-  //   if (send_bid == true) {
-  //     console.log("adding bid here pls work");
-  //     console.log(pet);
-  //     dispatch(addBid(
-  //       petowner_username,
-  //       pet,
-  //       caretaker_username,
-  //       start_date,
-  //       end_date,
-  //       parseInt(price),
-  //       transfer_method,
-  //       payment_method
-  //     ));
+  //   if (open) {
+  //     dispatch(getPetName(petowner_username, pet_type));
   //   }
-  // }, [send_bid]);
+  // }, [pet_type, caretaker]);
+
+  const today = new Date();
+  const today_date = `${today.getFullYear()}-${today.getMonth() + 1
+    }-${today.getDate()}`;
+  const two_years_later_date = `${today.getFullYear() + 2}-${today.getMonth() + 1
+    }-${today.getDate()}`;
 
   const bid = async () => {
+    console.log("pet name: " + pet_name);
+    console.log(petowner_username);
     await dispatch(
       addBid(
         petowner_username,
-        pet,
+        pet_name,
         caretaker,
         start_date,
         end_date,
@@ -84,108 +68,117 @@ export default function Bid(props) {
       )
     );
     onClose();
-    setSendBid(false);
   };
 
-  return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Bid page</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          Please enter these information before bidding
-        </DialogContentText>
-        <TextField
-          id="date"
-          label="Select start date"
-          type="date"
-          defaultValue=""
-          inputProps={{
-            min: today_date,
-            max: two_years_later_date,
-          }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={(e) => setStartDate(e.target.value)}
-        />
+  if (pet_names) {
+    return (
+      <Dialog open={open} onClose={onClose}>
+        <DialogTitle>Bid page</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter these information before bidding
+          </DialogContentText>
+          <TextField
+            id="date"
+            label="Select start date"
+            type="date"
+            defaultValue=""
+            inputProps={{
+              min: today_date,
+              max: two_years_later_date,
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
 
-        <TextField
-          id="date"
-          label="Select end date"
-          type="date"
-          defaultValue=""
-          inputProps={{
-            min: today_date,
-            max: two_years_later_date,
-          }}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          onChange={(e) => setEndDate(e.target.value)}
-        />
-        {/* <TextField
-          autoFocus
-          label="Price"
-          type="number"
-          fullWidth
-          inputProps={{
-            min: caretakerPrice,
-          }}
-          onChange={(e) => setPrice(e.target.value)}
-        /> */}
-        <FormControl className={classes.formControl}>
-          <InputLabel id="select-pet-type">Select pet type</InputLabel>
-          <Select
-            labelId="select-pet-type"
-            id="select-pet-type"
-            onChange={(e) => setPetType(e.target.value)}
-          >
-            <MenuItem value={"Cat"}>Cat</MenuItem>
-            <MenuItem value={"Dog"}>Dog</MenuItem>
-            <MenuItem value={"Hamster"}>Hamster</MenuItem>
-            <MenuItem value={"Terrapin"}>Terrapin</MenuItem>
-            <MenuItem value={"Bird"}>Bird</MenuItem>
-            <MenuItem value={"Rabbit"}>Rabbit</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel id="select-transfer-method">
-            Select transfer method
-          </InputLabel>
-          <Select
-            labelId="select-transfer-method"
-            id="select-transfer-method"
-            value={transfer_method}
-            onChange={(e) => setTransferMethod(e.target.value)}
-          >
-            <MenuItem value={"Delivered by pet owner"}>
-              Delivered by pet owner
-            </MenuItem>
-            <MenuItem value={"Caretaker pick up"}>Caretaker pick up</MenuItem>
-            <MenuItem value={"Transfer through PCS building"}>
-              Transfer through PCS building
-            </MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <InputLabel id="select-payment-method">
-            Select payment method
-          </InputLabel>
-          <Select
-            labelId="select-payment-method"
-            id="select-payment-method"
-            value={payment_method}
-            onChange={(e) => setPaymentMethod(e.target.value)}
-          >
-            <MenuItem value={"By cash"}>By cash</MenuItem>
-            <MenuItem value={"By credit card"}>By credit card</MenuItem>
-          </Select>
-        </FormControl>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={bid}>Bid</Button>
-      </DialogActions>
-    </Dialog>
-  );
+          <TextField
+            id="date"
+            label="Select end date"
+            type="date"
+            defaultValue=""
+            inputProps={{
+              min: today_date,
+              max: two_years_later_date,
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+          {/* <TextField
+            autoFocus
+            label="Price"
+            type="number"
+            fullWidth
+            inputProps={{
+              min: caretakerPrice,
+            }}
+            onChange={(e) => setPrice(e.target.value)}
+          /> */}
+          <FormControl className={classes.formControl}>
+            <InputLabel id="select-pet">Select pet</InputLabel>
+            <Select
+              labelId="select-pet"
+              id="select-pet"
+              onChange={(e) => setPetName(e.target.value)}
+            >
+              {pet_names.map((pet, i) => (
+                <MenuItem value={pet["pet_name"]}>{pet["pet_name"]}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="select-transfer-method">
+              Select transfer method
+            </InputLabel>
+            <Select
+              labelId="select-transfer-method"
+              id="select-transfer-method"
+              value={transfer_method}
+              onChange={(e) => setTransferMethod(e.target.value)}
+            >
+              <MenuItem value={"Delivered by pet owner"}>
+                Delivered by pet owner
+              </MenuItem>
+              <MenuItem value={"Caretaker pick up"}>Caretaker pick up</MenuItem>
+              <MenuItem value={"Transfer through PCS building"}>
+                Transfer through PCS building
+              </MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="select-payment-method">
+              Select payment method
+            </InputLabel>
+            <Select
+              labelId="select-payment-method"
+              id="select-payment-method"
+              value={payment_method}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            >
+              <MenuItem value={"By cash"}>By cash</MenuItem>
+              <MenuItem value={"By credit card"}>By credit card</MenuItem>
+            </Select>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button onClick={bid}>Bid</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  } else {
+    return (
+      <Dialog open={open} onClose={onClose}>
+        <DialogTitle>Bid page</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please add your pets before bidding
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    );
+  }
 }
