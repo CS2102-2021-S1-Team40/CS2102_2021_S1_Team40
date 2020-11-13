e5eb08950c735b306ef0aa9b27bc7abd0a48cf3a
weikiat97 <jerryloh97@hotmail.com>
2020-11-13 13:15:54 +0800

    Added fix

--- public/src/components/BidDecline.js
+++ public/src/components/BidDecline.js
@@ -13,8 +13,8 @@ export default function BidDecline(props) {
   const { open, onClose, data } = props;
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  const cancel = () => {
-    dispatch(
+  const cancel = async () => {
+    await dispatch(
       declineBid(
         data.split(",")[0].split("(")[1],
         data.split(",")[1],
